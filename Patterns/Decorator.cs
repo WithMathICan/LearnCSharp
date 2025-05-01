974af129a82077fb58a242dda9f27fb8dc320df8
Serge Dyachenko <smdyachenko@gmail.com>
2025-05-01 15:04:30 +0300

    Dispose pattern

---
+++ Patterns/Decorator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns {
+
+    interface IArithmeticOperations {
+        int Add(int x, int y);
+        int Multiply(int x, int y);
+    }
+
+    class AOImplementation : IArithmeticOperations {
+        public int Add(int x, int y) => x + y;
+        public int Multiply(int x, int y) => x * y;
+    }
+
+    class AOLoggerImplementation : IArithmeticOperations {
+        IArithmeticOperations BaseImplementation;
+
+        public AOLoggerImplementation(IArithmeticOperations arithmetic) {
+            BaseImplementation = arithmetic;
+        }
+
+        public int Add(int x, int y) {
+            Console.WriteLine("Add method called");
+            return BaseImplementation.Add(x, y);
+        }
+
+        public int Multiply(int x, int y) {
+            Console.WriteLine("Multiply method called");
+            return BaseImplementation.Multiply(x, y);
+        }
+    }
+
+    internal class Decorator {
+
+        //static void Main() {
+        //    IArithmeticOperations ao = new AOImplementation();
+        //    ao = new AOLoggerImplementation(ao);
+        //    Console.WriteLine(ao.Add(3, 4));
+        //}
+    }
+}
---
+++ Patterns/DisposePattern.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns {
+    internal class BaseClass : IDisposable {
+        private List<int> list;
+        private bool isDisposeDone = false;
+
+        internal BaseClass() {
+            Console.WriteLine("Constructor BaseClass");
+            list = new List<int>(1000);
+            for (int i = 0; i < 1000; i++) list.Add(i);
+        }
+
+        protected virtual void Dispose(bool disposing) {
+            if (!isDisposeDone) {
+                if (disposing) {
+                    Console.WriteLine("(Dispose) BaseClass");
+                    list.Clear();
+                    list.Capacity = 0;
+                }
+                Console.WriteLine("~BaseClass");
+                isDisposeDone = true;
+            }
+        }
+        ~BaseClass() {
+            Dispose(false);
+        }
+
+        public void Dispose() {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    internal class MainClass : BaseClass {
+        private List<int> list;
+        private bool isDisposeDone = false;
+
+        internal MainClass() {
+            Console.WriteLine("Constructor MainClass");
+            list = new List<int>(1000);
+            for (int i = 0; i < 1000; i++) list.Add(i);
+        }
+
+        protected override void Dispose(bool disposing) {
+            if (!isDisposeDone) {
+                if (disposing) {
+                    Console.WriteLine("(Dispose) MainClass");
+                    list.Clear();
+                    list.Capacity = 0;
+                }
+                Console.WriteLine("~MainClass");
+                isDisposeDone = true;
+            }
+            base.Dispose(disposing);
+        }
+        ~MainClass() {
+            Dispose(false);
+        }
+    }
+
+    internal class DisposePattern {
+        static void Main() {
+            using (new MainClass());
+        }
+    }
+}
